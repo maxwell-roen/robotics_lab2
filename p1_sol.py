7fdb82d4707babf395ebec064403f245cedce4e8
Maxwell Roen <maxwellroen@Maxwells-MacBook-Pro.local>
2022-02-14 21:56:01 -0500

    finishes implementing everything

--- p1_sol.py
+++ p1_sol.py
@@ -3,22 +3,43 @@ import numpy as np
 import math
 
 
-# not sure if this is correct.
-# FIXED FRAME -- should be left multiplies
-if __name__ == '__main__':
-	# declare values for x, y, and z rotations
-	theta = math.pi / 2
-	phi = math.pi / 2
-	psi = math.pi / 2
-	# declare a starting point, vector v0
-	v0 = rbm.vec(1, 1, 1)
+def calculate_fixed_frame_roll_pitch_yaw_rotation(starting_pt, psi, theta, phi):
+	"""
+	This function calculates the results of a roll-pitch-yaw rotation based on the given angles.
+	:param starting_pt: The initial position
+	:param psi: Rotation about x, the roll
+	:param theta: Rotation about y, the pitch
+	:param phi: Rotation about z, the yaw
+	:return: A vector containing the new position of the point after the rotations.
+	"""
 	# create the three rotation matrices using the rotation matrix formulas defined in the rbm module
 	Rx = rbm.rot_x(psi)
 	Ry = rbm.rot_y(theta)
 	Rz = rbm.rot_z(phi)
-	# do two right multiplications to calculate the total rotation with moving frames
-	R = np.matmul(Rx, Ry)
-	R = np.matmul(R, Rz)
-	# then calculate and display the results
-	v1 = R.dot(v0)
+
+	# do two left multiplications to calculate the total rotation with a fixed frame
+	H = np.matmul(Rz, (np.matmul(Ry, Rx)))
+
+	# then calculate and the result
+	updated_pt = np.matmul(H, starting_pt)
+
+	return updated_pt
+
+
+if __name__ == '__main__':
+	# rotation values from the problem statement:
+	theta = math.pi / 2
+	phi = math.pi / 2
+	psi = math.pi / 2
+
+	# a simple test case.
+	v0 = [1, 1, 1]
+	v1 = calculate_fixed_frame_roll_pitch_yaw_rotation(v0, theta, phi, psi)
+	# based on doing this by hand, we expect to get [1, 1, -1], and we do.
+	print(f'Result:\n{v1}\n')
+
+	# another example
+	v0 = [2, 4, 6]
+	v1 = calculate_fixed_frame_roll_pitch_yaw_rotation(v0, theta, phi, psi)
+	# based on doing this by hand, we expect this to give [6, 4, -2], and we do.
 	print(f'Result:\n{v1}')
--- p3_sol.py
+++ p3_sol.py
@@ -1,5 +1,6 @@
+# import our basic homogenous transformations from problem 2.
+# we don't use rot_y here.
 from p2_sol import rot_x
-from p2_sol import rot_y
 from p2_sol import rot_z
 from p2_sol import translate_x
 from p2_sol import translate_y
@@ -8,29 +9,139 @@ import math
 import numpy as np
 
 
-# first four sub problems should have the same solution, when checking answers.
+def get_magnitude_of_3d_vec(vec):
+	"""
+	Helper function to calculate the magnitude of a 3d vector.
+	Formula is sqrt(a^2 + b^2 + c^2)
+	:param vec: A 3d vector. 
+	:return: The magnitude of that vector.
+	"""
+	return math.sqrt(math.pow(vec[0], 2) + math.pow(vec[1], 2) + math.pow(vec[2], 2))
+
+
 def H_1(v0):
 	"""
-	Transormation from from problem 3.
+	Transformation H_1 from problem 3.
+	:param v0: an initial point
+	:return v1: the updated position
 	"""
-	# follow the invidivual steps to create the single homogenous transformation
+	# follow the individual steps to create the single homogenous transformation
+	# we use right multiplies as we are using moving frames.
 	H = translate_x(2.5)
 	H = np.matmul(H, translate_z(0.5))
 	H = np.matmul(H, translate_y(-1.5))
-	# multiply the initial vector by H to produce the result
-	#v1 = H.dot(v0)
-	# oh, it's supposed to be left multiply? I think? is that the issue?
-	# idk, my linear algreba is weak/nonexistent, need to go learn that. let's try it though:
-	#v1 = v0.dot(H)
-	# that doesn't work. how about:
-	v1 = np.matmul(v0, H)
-	# that seems to be wrong, too. come back tomorrow;
-	
+
+	# Multiply H*v0 to get resulting point
+	v1 = np.matmul(H, v0)
+
+	return v1
+
+
+def H_2(v0):
+	"""
+	Transformation H_2 from problem 3
+	:param v0: an initial point
+	:return v1: the updated position
+	"""
+	# follow the individual steps to create the single homogenous transformation
+	# we use right multiplies as we are using moving frames.
+	H = translate_z(0.5)
+	H = np.matmul(H, translate_x(2.5))
+	H = np.matmul(H, translate_y(-1.5))
+
+	# Multiply H*v0 to get resulting point
+	v1 = np.matmul(H, v0)
+
+	return v1
+
+
+def H_3(v0):
+	"""
+	Transformation H_3 from problem 3
+	:param v0: an initial point
+	:return v1: the updated position
+	"""
+	# follow the individual steps to create the single homogenous transformation
+	# we use left multiplies as we are using a fixed frame.
+	H = translate_x(2.5)
+	H = np.matmul(translate_z(0.5), H)
+	H = np.matmul(translate_y(-1.5), H)
+
+
+	# Multiply H*v0 to get resulting point
+	v1 = np.matmul(H, v0)
+
+	return v1
+
+
+def H_4(v0):
+	"""
+	Transformation H_4 from problem 3
+	:param v0: an initial point
+	:return v1: the updated position
+	"""
+	# follow the individual steps to create the single homogenous transformation
+	# we use left multiplies as we are using a fixed frame.
+	H = translate_z(0.5)
+	H = np.matmul(translate_x(2.5), H)
+	H = np.matmul(translate_y(-1.5), H)
+
+	# Multiply H*v0 to get resulting point
+	v1 = np.matmul(H, v0)
+
+	return v1
+
+
+def H_5(v0):
+	"""
+	Transformation H_5 from problem 3
+	:param v0: an initial point
+	:return v1: the updated position
+	"""
+	# follow the individual steps to create the single homogenous transformation
+	# we use right multiplies as we are using moving frames.
+	H = rot_x(math.pi / 2)
+	H = np.matmul(H, translate_x(3))
+	H = np.matmul(H, translate_z(-3))
+	H = np.matmul(H, rot_z(-math.pi / 2))
+
+	# multiple H*v0 to get the resulting point
+	v1 = np.matmul(H, v0)
+
 	return v1
 
 
 if __name__ == '__main__':
-    v0 = [0, 0, 0, 0]
-    v1 = H_1(v0)
-    print(v1)
+	# declare a starting point
+	v0 = [1, 1, 1]
+	# append the magnitude to the vector so we can multiply by H, which will be 4x4
+	v0.append(get_magnitude_of_3d_vec(v0))
 
+	# set some numpy print options for the output. Suppress forces numpy to not use scientific notation
+	# for small values.
+	np.set_printoptions(precision=2, suppress=True)
+
+	# test the H_1 transformation
+	v1 = H_1(v0)
+	# and this matches what we got by hand.
+	print(v1)
+
+	# test the H_2 transformation
+	v2 = H_2(v0)
+	# which matches our result from H_1 -- the first four transformations should have the same result, we're good to go.
+	print(v2)
+
+	# test the H_3 transformation
+	v3 = H_3(v0)
+	# again, good to go.
+	print(v3)
+
+	# test the H_4 transformation
+	v4 = H_3(v0)
+	# looks good.
+	print(v4)
+
+	# finally, test the H_5 transformation. gotta work this one by hand too.
+	v5 = H_5(v0)
+	# we are expecting to get: [6.2, 4.2, -1, sqrt(3)]. and we do.
+	print(v5)
